c25365f8d3565694991658630c66c4f606c9ae88
Ananya Hans <ananyahans@Ananyas-iMac.centurylink.net>
2021-03-01 18:31:25 -0600

    cookies

--- public/js/cookies.js
+++ public/js/cookies.js
@@ -8,5 +8,50 @@ function setCookie(name, value){
   }
 
 setCookie("ananya_cookie", "doggydoodoo");
+setCookie("eddy_cookie", "pigdoodoo");
+setCookie("jimmy_cookie", "catdoodoo");
+
+
+function getCookie(){
+  let array = document.cookie.split("; ")
+  return array
+}
+
+// console.log(getCookie())
+
+function getCookieValue(name){
+  let array = getCookie()
+  let value = null
+  for(let i = 0; i < array.length; i++){
+    let element = array[i]
+      if(element.includes(name)){
+        value = (element.slice(name.length + 1, element.length))
+      }
+
+    }
+  
+return value
+  
+}
+
+// console.log(getCookieValue("rabbit"));
+// console.log(getCookieValue("her-cookie"));
+// console.log(getCookieValue("eddy_cookie"));
+
+function deleteCookie(arg){
+  let array = getCookie()
+  for(let i = 0; i < array.length; i++){
+    let element = array[i]
+      if(element.includes(arg)){
+        array = array.splice(array.indexOf(arg), 1)
+      } else {
+        console.log("COOKIE NOT FOUND!")
+      }
+
+  }
+}
+console.log(deleteCookie("rabbit"));
+console.log(deleteCookie("her-cookie"));
+console.log(deleteCookie("eddy_cookie"));
 
 });
